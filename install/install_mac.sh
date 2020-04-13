051c7cd7cae21336b85d19614277f5181de80283
Mark Harris <mharris@technobear.com>
2020-04-13 15:22:02 +0200

    mac : check for homebrew installation, and just upgrade if necessary

--- install/install_mac.sh
+++ install/install_mac.sh
@@ -2,8 +2,43 @@
 set -e
 
 #install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+which brew > /dev/null 2>&1
+if [ $? -ne 0 ]; then 
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    brew update
+fi
 
-#install required tools
-brew install arm-linux-gnueabihf-binutils llvm rsync coreutils
+#install or upgrade required tools
+BOTDIR=~/Library/Caches/Homebrew
+
+ls $BOTDIR/rsync* > /dev/null 2>&1
+if [ $? -ne 0 ]; then 
+    brew install rsync
+else
+    brew upgrade rsync
+fi
+
+ls $BOTDIR/coreutils* > /dev/null 2>&1
+if [ $? -ne 0 ]; then 
+    brew install coreutils
+else
+    brew upgrade coreutils
+fi
+
+ls $BOTDIR/arm-linux-gnueabihf-binutils* > /dev/null 2>&1
+if [ $? -ne 0 ]; then 
+    brew install arm-linux-gnueabihf-binutils
+else
+    brew upgrade arm-linux-gnueabihf-binutils
+fi
+
+ls $BOTDIR/llvm* > /dev/null 2>&1
+if [ $? -ne 0 ]; then 
+    brew install llvm
+else
+    brew upgrade llvm
+fi
+
+
 
